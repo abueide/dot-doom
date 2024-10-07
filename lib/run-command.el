aa16fdc2fd9a3adde567744099a93aec685460f4
Andrea Bueide <abueide@natea.io>
2024-10-06 21:45:21 -0500

    multishell support

--- lib/run-command.el
+++ lib/run-command.el
@@ -11,52 +11,55 @@
 
 (defvar run-command-debug t "Enable debug for run-command.")
 (defvar run-command-buffer-policy "onSuccess"
-  "Output Buffer Policy: always, never, onSuccess, onFailure.")
+  "Output buffer policy: always, never, onSuccess, onFailure.")
+(defvar run-command-shell "bash"
+  "Default shell to use for running commands. Can be set to bash, fish, or zsh.")
 
 (defun run-command--log (message &rest args)
   "Log MESSAGE with ARGS if `run-command-debug` is enabled."
   (when run-command-debug
     (apply #'message (concat "[run-command]: " message) args)))
 
-(defun run-command (command)
-  "Run COMMAND in a term buffer."
-  (run-command--log "Running: %s" command)
-  (let* ((buffer-name "*run-command-output*")
-         (buffer (get-buffer-create buffer-name)))
-    (run-command--log "Creating term buffer: %s" buffer-name)
-    (with-current-buffer buffer
-      (term-mode)
-      (term-exec buffer "run-command" "bash" nil (list "-l" "-i" "-c" command)))
-    (display-buffer-at-bottom buffer '((window-height . 0.25)))
-    (select-window (get-buffer-window buffer))
-    (evil-normal-state)
-    (set-process-sentinel (get-buffer-process buffer)
-                          (lambda (proc _event)
-                            (let ((exit-code (process-exit-status proc)))
-                              (run-command--log "Exit code: %d" exit-code)
-                              (cond
-                               ((string= run-command-buffer-policy "always")
-                                (run-command--log "Policy: always keep buffer."))
-                               ((string= run-command-buffer-policy "never")
-                                (run-command--log "Policy: never keep buffer.")
-                                (delete-window (get-buffer-window buffer)))
-                               ((and (string= run-command-buffer-policy "onSuccess")
-                                     (= exit-code 0))
-                                (run-command--log "Policy: onSuccess, closing buffer.")
-                                (delete-window (get-buffer-window buffer)))
-                               ((and (string= run-command-buffer-policy "onFailure")
-                                     (/= exit-code 0))
-                                (run-command--log "Policy: onFailure, closing buffer.")
-                                (delete-window (get-buffer-window buffer)))))))))
+(defun run-command (command &optional shell)
+  "Run COMMAND in a term buffer using SHELL."
+  (let ((shell-to-use (or shell run-command-shell)))
+    (run-command--log "Running: %s with shell: %s" command shell-to-use)
+    (let* ((buffer-name "*run-command-output*")
+           (buffer (get-buffer-create buffer-name)))
+      (run-command--log "Creating term buffer: %s" buffer-name)
+      (with-current-buffer buffer
+        (term-mode)
+        (term-exec buffer "run-command" shell-to-use nil (list "-l" "-i" "-c" command)))
+      (display-buffer-at-bottom buffer '((window-height . 0.25)))
+      (select-window (get-buffer-window buffer))
+      (evil-normal-state)
+      (set-process-sentinel (get-buffer-process buffer)
+                            (lambda (proc _event)
+                              (let ((exit-code (process-exit-status proc)))
+                                (run-command--log "Exit code: %d" exit-code)
+                                (cond
+                                 ((string= run-command-buffer-policy "always")
+                                  (run-command--log "Policy: always keep buffer."))
+                                 ((string= run-command-buffer-policy "never")
+                                  (run-command--log "Policy: never keep buffer.")
+                                  (delete-window (get-buffer-window buffer)))
+                                 ((and (string= run-command-buffer-policy "onSuccess")
+                                       (= exit-code 0))
+                                  (run-command--log "Policy: onSuccess, closing buffer.")
+                                  (delete-window (get-buffer-window buffer)))
+                                 ((and (string= run-command-buffer-policy "onFailure")
+                                       (/= exit-code 0))
+                                  (run-command--log "Policy: onFailure, closing buffer.")
+                                  (delete-window (get-buffer-window buffer))))))))))
 
-(defun run-selected-command ()
-  "Run the currently selected text as a command."
+(defun run-selected-command (&optional shell)
+  "Run the currently selected text in SHELL."
   (interactive)
   (run-command--log "Running selected command.")
   (if (use-region-p)
       (let ((command (buffer-substring-no-properties (region-beginning) (region-end))))
         (run-command--log "Selected command: %s" command)
-        (run-command command))
+        (run-command command shell))
     (run-command--log "No region selected.")))
 
 ;;; Unit tests for run-command functions using ERT
@@ -85,25 +88,28 @@
   "Test the exit code and output of specific commands."
   (let ((buffer-name "*run-command-output*")
         (run-command-buffer-policy "always"))
-    ;; Success case: "echo test"
-    (run-command "echo test")
+    ;; Success case: "echo test" with bash
+    (run-command "echo test" "bash")
     (with-current-buffer buffer-name
       (goto-char (point-max))
       (should (search-backward "test" nil t))
       (should (= (process-exit-status (get-buffer-process buffer-name)) 0)))
-    ;; Failure case: "colmena fdh"
-    (run-command "colmena fdh")
+    ;; Success case: "echo test" with fish
+    (run-command "echo test" "fish")
     (with-current-buffer buffer-name
       (goto-char (point-max))
-      (should (search-backward "unrecognized subcommand" nil t))
-      (should (/= (process-exit-status (get-buffer-process buffer-name)) 0)))))
+      (should (search-backward "test" nil t))
+      (should (= (process-exit-status (get-buffer-process buffer-name)) 0)))
+    ;; Success case: "echo test" with zsh
+    (run-command "echo test" "zsh")
+    (with-current-buffer buffer-name
+      (goto-char (point-max))
+      (should (search-backward "test" nil t))
+      (should (= (process-exit-status (get-buffer-process buffer-name)) 0)))));;; run-command.el ends here;;
 
-(provide 'run-command)
-;;; run-command.el ends here;;
 ;; echo test
 ;; colmena fh
 ;; (run-command-tests)
 ;; (ert t)
 ;;
 ;; (setq debug-on-error t)
-;;
